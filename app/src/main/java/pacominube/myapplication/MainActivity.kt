1a25d649e2e4adceef659441fea31a01b1f94d78
Paco Minube <franciscoalfacemartin@gmail.com>
2018-09-20 16:02:38 +0200

    added login logic

--- app/src/main/java/pacominube/myapplication/MainActivity.kt
+++ app/src/main/java/pacominube/myapplication/MainActivity.kt
@@ -1,12 +1,24 @@
 package pacominube.myapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val CREDENTIALS = "admin"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.setOnClickListener {
+            if(userName.text.toString() != CREDENTIALS || password.text.toString() != CREDENTIALS) {
+                Toast.makeText(this, "Usuario o contraseña inválidos", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
